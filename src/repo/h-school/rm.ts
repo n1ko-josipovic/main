cbf9399e5279dc4b13bd8b87f41d9110ae71ce70
Niko Josipović <144704565+n1ko-josipovic@users.noreply.github.com>
2024-10-31 22:58:02 +0100

    Update rm.ts

--- src/repo/h-school/rm.ts
+++ src/repo/h-school/rm.ts
@@ -12,7 +12,7 @@ const createRm = (): string[] => {
         "<br>",
         `&nbsp;&nbsp;<span class='${ARROW_COLOR} ${SHADOW}'>→</span>&nbsp;<a href="repo - sadržaj/3. razred/RM/LV01.pdf" target="_blank">LV01 - Enkapsulacija podataka kroz slojeve OSI modela</a>`,
         `&nbsp;&nbsp;<span class='${ARROW_COLOR} ${SHADOW}'>→</span>&nbsp;<a href="repo - sadržaj/3. razred/RM/LV02.pdf" target="_blank">LV02 - Osnovna analiza mrežnog prometa</a>`,
-        `&nbsp;&nbsp;<span class='${ARROW_COLOR} ${SHADOW}'>→</span>&nbsp;<a href="repo - sadržaj/3. razred/RM/LV03.docx" target="_blank">LV03 - Subnetiranje & Subnetiranje pomoću VLSM tehnike</a>`,
+        `&nbsp;&nbsp;<span class='${ARROW_COLOR} ${SHADOW}'>→</span>&nbsp;<a href="repo - sadržaj/3. razred/RM/LV03.pdf" target="_blank">LV03 - Subnetiranje & Subnetiranje pomoću VLSM tehnike</a>`,
         `&nbsp;&nbsp;<span class='${ARROW_COLOR} ${SHADOW}'>→</span>&nbsp;<a href="repo - sadržaj/3. razred/RM/LV04.docx" target="_blank">LV04 - Prikaz računalne mreže s usmjernikom i preklopnicima</a>`,
         "<br>",
         `&nbsp;<span class='${LOWLIGHTED}'>Ostalo:</span>`,
